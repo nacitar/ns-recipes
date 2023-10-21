fb241c3d0e1fb6757f6ce34bc502a655f79e1b59
Jacob McIntosh <nacitar.sevaht@gmail.com>
2023-10-20 19:55:10 -0600

    Added last step for classic chili

--- recipes/classic_chili.py
+++ recipes/classic_chili.py
@@ -22,8 +22,9 @@ recipe = Recipe(
         "1/2 teaspoon ground cayenne pepper",
     ],
     instructions=[
-        "Brown the ground beef in a skillet.  Drain fat and return to skillet.",
-        "Add all of the ingredients to the skillet.  Stir until well combined.",
-        "Bring to a low boil, reduce the heat, and simmer uncovered for 25 minutes, stirring occasionally."
+        "Brown the ground beef in a skillet. Drain fat and return to skillet.",
+        "Add all of the ingredients to the skillet. Stir until well combined.",
+        "Bring to a low boil, reduce the heat, and simmer uncovered for 25 minutes, stirring occasionally.",
+        "Remove from heat. Let rest for 5-10 minutes before serving.",
     ]
 )
